92c03cd4c24518727d2f38524a05341f4f31b925
Alexandre <contact@acanoen.fr>
2020-06-29 16:25:07 +0200

    Send e-mail in contact form

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,28 @@ menuToggle.addEventListener('click', event => {
     else
         menu.classList.remove('toggle')
 })
+
+/* When user submit contact form */
+let contactForm = document.querySelector('#contact-form')
+contactForm.addEventListener('submit', event => {
+    event.preventDefault()
+    window.fetch('/email/send', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify({
+            name: event.target.name.value,
+            email: event.target.email.value,
+            subject: event.target.subject.value,
+            message: event.target.message.value
+        })
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            let alert = document.querySelector('#alert')
+            alert.innerHTML = res.message
+            alert.style = `color: ${res.isSend ? '#4CAF50' : '#F44336'}`
+        })
+})
--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use \Symfony\Component\HttpFoundation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BaseController extends AbstractController
@@ -21,6 +23,43 @@ class BaseController extends AbstractController
     public function contact()
     {
         return $this->render('base/contact.html.twig');
+    }
+
+    /**
+     * @Route("/email/send", name="base_email_send", methods={"POST"})
+     * @param Request $request
+     * @return HttpFoundation\JsonResponse
+     */
+    public function sendEmail(Request $request)
+    {
+        $req = json_decode($request->getContent(), true);
+        $isSuccess = true;
+        $error = '';
+
+        if (!isset($req['name']) && !isset($req['email']) && !isset($req['subject']) && !isset($req['message'])) {
+            $isSuccess = false;
+            $error = 'Tous les champs doivent être renseignés.';
+        }
+
+        if (!filter_var($req['email'], FILTER_VALIDATE_EMAIL)) {
+            $isSuccess = false;
+            $error = 'L\'adresse e-mail n\'est pas renseigné dans le bon format.';
+        }
+
+        if ($isSuccess) {
+            $headers = 'MIME-Version: 1.0' . "\r\n" .
+                'Content-Type: text/html; charset=UTF-8' . "\r\n" .
+                "From: {$req['name']} <{$req['email']}>" . "\r\n" .
+                "Reply-To: {$req['name']} <{$req['email']}>" . "\r\n";
+            @mail('contact@acanoen.fr', $req['subject'], $req['message'], $headers);
+            $error = 'Votre demande a bien été envoyée et sera traitée le plus rapidement possible.';
+        }
+
+        return $this->json([
+            'isSend' => $isSuccess,
+            'message' => $error,
+            'data' => $req
+        ]);
     }
 
     /**
