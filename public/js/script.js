d614360d5157ea79c07730c5382ba05db187888f
Alexandre Canoën <contact@acanoen.fr>
2020-11-05 19:49:21 +0100

    Add front office (for desktop)

--- public/js/script.js
+++ public/js/script.js
@@ -1,43 +1,28 @@
-/* When user scroll page */
-document.addEventListener('scroll', event => {
-    let header = document.querySelector('.header')
+let slideIndex = 1
+
+document.addEventListener('scroll', evt => {
+    let nav = document.querySelector('.nav')
     if (window.scrollY > 0)
-        header.classList.add('bg')
+        nav.classList.add('bg')
     else
-        header.classList.remove('bg')
+        nav.classList.remove('bg')
 })
 
-/* When user click on menu */
-let menuToggle = document.querySelector('.menu-toggle')
-menuToggle.addEventListener('click', event => {
-    let menu = document.querySelector('.menu')
-    if (!menu.classList.contains('toggle'))
-        menu.classList.add('toggle')
-    else
-        menu.classList.remove('toggle')
-})
+const pushSlide = n => {
+    showSlide(slideIndex += n)
+}
 
-/* When user submit contact form */
-let contactForm = document.querySelector('#contact-form')
-contactForm.addEventListener('submit', event => {
-    event.preventDefault()
-    window.fetch('/email/send', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8'
-        },
-        body: JSON.stringify({
-            name: event.target.name.value,
-            email: event.target.email.value,
-            subject: event.target.subject.value,
-            message: event.target.message.value
-        })
-    })
-        .then(res => res.json())
-        .then(res => {
-            console.log(res)
-            let alert = document.querySelector('#alert')
-            alert.innerHTML = res.message
-            alert.style = `color: ${res.isSend ? '#4CAF50' : '#F44336'}`
-        })
-})
+const showSlide = n => {
+    let i
+    const slides = document.getElementsByClassName("slide")
+    if (n > slides.length)
+        slideIndex = 1
+    if (n < 1)
+        slideIndex = slides.length
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+    slides[slideIndex-1].style.display = "block"
+}
+
+showSlide(slideIndex)
